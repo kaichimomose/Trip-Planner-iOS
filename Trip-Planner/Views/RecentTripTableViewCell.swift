03faedbdb0014b2913f63abf8e5a4ed2af4147cd
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-12-23 22:33:56 +0900

    added recenttrip

---
+++ Trip-Planner/Views/RecentTripTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecentTripTableViewCell.swift
+//  Trip-Planner
+//
+//  Created by Kaichi Momose on 2017/12/23.
+//  Copyright © 2017 Kaichi Momose. All rights reserved.
+//
+
+import UIKit
+
+class RecentTripTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
